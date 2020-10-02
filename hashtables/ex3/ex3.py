d9b973dad006eee660b731c5d4f4f12272ce72ef
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2020-10-02 14:20:38 -0400

    just added explanations

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,17 +2,23 @@ def intersection(arrays):
     result = []
     cache = {}
     arr = []
+    #add elements from arrays to arr empty list 
     for array in arrays:
         arr = arr + array
+        #iterate through new list then if item in cache add a plus one 
     for item in arr:
         if item in cache:
+            #set value to add to one 
             cache[item] += 1
         else:
+            #else just set to one one 
             cache[item] = 1
 
-       
+       # iterate through the keys of the dic 
     for key in cache.keys():
+        #if any given key (actual number) equals the length arrays (which is number of arrays)
         if cache[key] == len(arrays):
+            # append to result 
             result.append(key)
 
     return result
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,11 +4,16 @@ def has_negatives(a):
     """
     b = {}
     result = []
+    #for every element of a we are going to cache the key as the number and the value as the negative of that number
     for number in a:
         b[number] = -number
+        #
     for number in a:
+        #for every number in a 
         if -number in b:
+            #and if that number has a negative in cache b 
             if number > 0:
+                #and if the number in a is greater than 0(positive) add that to the results 
                 result.append(number)
 
     return result
